b30147f1f45e6827bea5fb545247ee9c7f834d23
sschr15 <steven.sschr15@hotmail.com>
2021-04-04 10:48:38 -0500

    Auto-commited by script

--- update.py
+++ update.py
@@ -11,6 +11,24 @@ recursive_dict = Dict[str, List[str]]
 
 json_struct: recursive_dict = {}
 
+html_template = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{0} on concernmaven</title>
+    </head>
+    <body>
+        <h1>Index of {0}</h1>
+        <ul>
+            {1}
+        </ul>
+    </body>
+</html>
+"""
+
+def create_element(filename: str) -> str:
+    return '<li><a href="./{0}">{0}</a></li>'.format(filename)
+
 for root, folders, files in os.walk("."):
     root: str = root.replace("\\", "/")
     folders: List[str] = [i for i in folders if not i.startswith(".")]
@@ -22,7 +40,14 @@ for root, folders, files in os.walk("."):
         files = []
         root = "./"
 
-    json_struct[root] = folders + files
+    elements = folders + files
+    
+    with open(root + "/index.html", "w") as file:
+        print("Writing index.html to " + root)
+        file.write(html_template.format(
+                root[root.rindex("/")+1:],
+                "\n            ".join([create_element(i) for i in elements])
+        ))
 
 with open("tree.json", "w") as json_file:
     json.dump(json_struct, json_file)
